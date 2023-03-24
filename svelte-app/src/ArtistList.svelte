1d0ec2a2fee9c6a796ac1e44891ad11a5f3f52b1
marciofao <marcio.lopes.fao@gmail.com>
2023-03-24 13:20:05 -0300

    exploding array as variables  in foreach

--- svelte-app/src/ArtistList.svelte
+++ svelte-app/src/ArtistList.svelte
@@ -2,18 +2,14 @@
   export let artists;
 </script>
 
-{#each artists as artist}
+{#each artists as { name, src, reknown }}
   <div
     class="list-group-item d-flex w-100 list-group-item-action align-items-center"
   >
-    <img
-      class="rounded nr-3 d-block"
-      src={artist.src}
-      alt={`Photo of ${artist.name}`}
-    />
+    <img class="rounded nr-3 d-block" {src} alt={`Photo of ${name}`} />
     <h2 class="artist-name mb-0 text-primary">
-      {artist.name}
+      {name}
     </h2>
-    <p class="artist-reknown text-muted mb-0">{artist.reknown}</p>
+    <p class="artist-reknown text-muted mb-0">{reknown}</p>
   </div>
 {/each}
