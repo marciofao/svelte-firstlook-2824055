99da3d894532355fc9583a9c7b44dc17ba63bfc1
marciofao <marcio.lopes.fao@gmail.com>
2023-03-24 13:18:37 -0300

    iterating source of data

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -2,11 +2,23 @@
   import ArtistList from "./ArtistList.svelte";
   import ArtistSearch from "./ArtistSearch.svelte";
   let searchTerm = "Barot Bellingham";
+  let artists = [
+    {
+      name: "Barot Bellingham",
+      src: "https://lil-cdn.com/2824055/Barot_Bellingham_tn.jpg",
+      reknown: "Royal Academy of Painting and Sculpture",
+    },
+    {
+      name: "Jonathan G. Ferrar II",
+      src: "https://lil-cdn.com/2824055/Jonathan_Ferrar_tn.jpg",
+      reknown: "Artist to Watch in 2012",
+    },
+  ];
 </script>
 
 <div class="container">
   <ArtistSearch {searchTerm} />
-  <ArtistList />
+  <ArtistList {artists} />
 </div>
 
 <style global lang="scss">
--- svelte-app/src/ArtistList.svelte
+++ svelte-app/src/ArtistList.svelte
@@ -1,11 +1,19 @@
 <script>
-  let name = "Royal academy of painting";
-  let src = "https://lil-cdn.com/2824055/Barot_Bellingham_tn.jpg";
-  let reknown = "Royal Academy";
+  export let artists;
 </script>
 
-<img {src} alt={`Photo of ${name}`} />
-<h2 class="artist-name mb-0 text-primary">
-  {name}
-</h2>
-<p class="artist-reknown text-muted mb-0">{reknown}</p>
+{#each artists as artist}
+  <div
+    class="list-group-item d-flex w-100 list-group-item-action align-items-center"
+  >
+    <img
+      class="rounded nr-3 d-block"
+      src={artist.src}
+      alt={`Photo of ${artist.name}`}
+    />
+    <h2 class="artist-name mb-0 text-primary">
+      {artist.name}
+    </h2>
+    <p class="artist-reknown text-muted mb-0">{artist.reknown}</p>
+  </div>
+{/each}
