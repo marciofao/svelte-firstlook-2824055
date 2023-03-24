0f64d3d887ea7d9e38a92d2fee455673dd482301
marciofao <marcio.lopes.fao@gmail.com>
2023-03-24 13:03:21 -0300

    web components and props

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import ArtistList from "./ArtistList.svelte";
+  import ArtistSearch from "./ArtistSearch.svelte";
+  let searchTerm = "Barot Bellingham";
+</script>
+
+<div class="container">
+  <ArtistList {searchTerm} />
+</div>
+
+<style global lang="scss">
+  $primary: purple;
+
+  @import "../node_modules/bootstrap/scss/bootstrap.scss";
+</style>
---
+++ svelte-app/src/ArtistList.svelte
@@ -0,0 +1,11 @@
+<script>
+  let name = "royal academy of painting";
+  let src = "https://lil-cdn.com/2824055/Barot_Bellingham_tn.jpg";
+  let reknown = "royal Academy";
+</script>
+
+<img {src} alt={`Proto of ${name}`} />
+<h2 class="artist-name mb-0">
+  {name}
+</h2>
+<p class="artist-reknown text-muted mb-0">{reknown}</p>
