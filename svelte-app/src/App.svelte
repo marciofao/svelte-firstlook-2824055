822706629bf59cf3eaf73d1d26f58f8aefcf6da5
marciofao <marcio.lopes.fao@gmail.com>
2023-03-24 15:14:48 -0300

    presentation fix

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,8 +1,14 @@
 <script>
+  import { onMount } from "svelte";
   import ArtistList from "./ArtistList.svelte";
   import ArtistSearch from "./ArtistSearch.svelte";
   let searchTerm = "Barot Bellingham";
   let artists = [];
+
+  onMount(async () => {
+    const res = await fetch(`data.json`);
+    artists = await res.json();
+  });
 </script>
 
 <div class="container">
@@ -12,6 +18,5 @@
 
 <style global lang="scss">
   $primary: purple;
-
   @import "../node_modules/bootstrap/scss/bootstrap.scss";
 </style>
